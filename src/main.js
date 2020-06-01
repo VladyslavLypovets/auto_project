3a46a0f7aa70f3b74cfa6a9749ca09835b3adbf9
Vladislav Lypivets <avasl333@gmail.com>
2020-06-01 14:01:05 +0300

    add fonts

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.min.css'
-import '@/assets/fonts/stylesheet.css'
+import '@/assets/fonts/clearsans.css'
 import '@/assets/css/global.css'
 
 Vue.config.productionTip = false
