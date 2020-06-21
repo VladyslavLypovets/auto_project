6764a67f1a1af351a2cdbe983dfc3e6dbee632d5
Vladislav Lypivets <avasl333@gmail.com>
2020-06-21 14:57:02 +0300

    #fixed

---
+++ src/components/layout/RangeSlider.vue
@@ -0,0 +1,42 @@
+<template>
+  <vue-range-slider
+    ref="slider"
+    :value="value"
+    :max="900000"
+    :height="2"
+    @input="(val) => $emit('input', val)"
+  />
+</template>
+
+<script>
+import 'vue-range-component/dist/vue-range-slider.css'
+import VueRangeSlider from 'vue-range-component'
+
+export default {
+  name: 'RangeSlider',
+  components: {
+    VueRangeSlider
+  },
+  props: {
+    value: Array
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  /deep/.slider-dot {
+    height: 24px !important;
+    width: 9px !important;
+    border-radius: 4px !important;
+    top: -11px !important;
+    background-color: #00b9e5 !important;
+    box-shadow: none !important;
+  }
+  /deep/ .slider-process {
+    background-color: #00b9e5 !important;
+  }
+  /deep/ .slider-tooltip-top {
+    opacity: 0;
+    pointer-events: none;
+  }
+</style>
