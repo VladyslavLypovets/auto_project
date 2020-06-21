c90e1c5b78a4322f74115f97807383a06e03c2a7
Vladislav Lypivets <avasl333@gmail.com>
2020-06-21 10:07:01 +0300

    merge

--- src/pages/Catalog.vue
+++ src/pages/Catalog.vue
@@ -81,8 +81,14 @@
                     />
                   </multiselect>
                 </div>
-                <div class="manufacturer">
-                  <div class="name-manufacturer d-flex align-items-center justify-content-between">
+                <div
+                  class="manufacturer collapse-section"
+                  :class="{'open': collapsedMenu[0]}"
+                >
+                  <div
+                    class="name-manufacturer d-flex align-items-center justify-content-between"
+                    @click="collapsedMenu[0] = !collapsedMenu[0]"
+                  >
                     <h6>Производитель</h6>
                     <i class="fa fa-angle-down" aria-hidden="true"></i>
                   </div>
@@ -92,21 +98,27 @@
                       <label for="choice-manufacturer-1">Производитель 1</label>
                     </div>
                     <div class="d-flex align-items-center">
-                      <input type="checkbox" id="choice-manufacturer-1" class="form-control checkbox-manufacturer">
-                      <label for="choice-manufacturer-1">Производитель 1</label>
+                      <input type="checkbox" id="choice-manufacturer-2" class="form-control checkbox-manufacturer">
+                      <label for="choice-manufacturer-2">Производитель 2</label>
                     </div>
                     <div class="d-flex align-items-center">
-                      <input type="checkbox" id="choice-manufacturer-1" class="form-control checkbox-manufacturer">
-                      <label for="choice-manufacturer-1">Производитель 1</label>
+                      <input type="checkbox" id="choice-manufacturer-3" class="form-control checkbox-manufacturer">
+                      <label for="choice-manufacturer-3">Производитель 3</label>
                     </div>
                     <div class="d-flex align-items-center">
-                      <input type="checkbox" id="choice-manufacturer-1" class="form-control checkbox-manufacturer">
-                      <label for="choice-manufacturer-1">Производитель 1</label>
+                      <input type="checkbox" id="choice-manufacturer-4" class="form-control checkbox-manufacturer">
+                      <label for="choice-manufacturer-4">Производитель 4</label>
                     </div>
                   </div>
                 </div>
-                <div class="price">
-                  <div class="name-price d-flex align-items-center justify-content-between">
+                <div
+                  class="price collapse-section"
+                  :class="{'open': collapsedMenu[1]}"
+                >
+                  <div
+                    class="name-price d-flex align-items-center justify-content-between"
+                    @click="collapsedMenu[1] = !collapsedMenu[1]"
+                  >
                     <h6>Цена</h6>
                     <i class="fa fa-angle-down" aria-hidden="true"></i>
                   </div>
@@ -124,8 +136,14 @@
                     </div>
                   </div>
                 </div>
-                <div class="attribute">
-                  <div class="name-attribute d-flex align-items-center justify-content-between">
+                <div
+                  class="attribute collapse-section"
+                  :class="{'open': collapsedMenu[2]}"
+                >
+                  <div
+                    class="name-attribute d-flex align-items-center justify-content-between"
+                    @click="collapsedMenu[2] = !collapsedMenu[2]"
+                  >
                     <h6>Атрибут 1</h6>
                     <i class="fa fa-angle-down" aria-hidden="true"></i>
                   </div>
@@ -144,8 +162,14 @@
                     </div>
                   </div>
                 </div>
-                <div class="attribute">
-                  <div class="name-attribute d-flex align-items-center justify-content-between">
+                <div
+                  class="attribute collapse-section"
+                  :class="{'open': collapsedMenu[3]}"
+                >
+                  <div
+                    class="name-attribute d-flex align-items-center justify-content-between"
+                    @click="collapsedMenu[3] = !collapsedMenu[3]"
+                  >
                     <h6>Атрибут 2</h6>
                     <i class="fa fa-angle-down" aria-hidden="true"></i>
                   </div>
@@ -167,25 +191,41 @@
                 <div class="d-flex align-items-center">
                   <h6>Сортировать:</h6>
                   <div class="output-by">
-                    <select class="nice-select  select-output-by">
-                      <option value="рейтингу">По рейтингу</option>
-                      <option value="Акционные">Акционные</option>
-                      <option value="новизне">По новизне</option>
-                      <option value="популярности">По популярности</option>
-                      <option value="Отдешевых">От дешевых к дорогим</option>
-                      <option value="Отдорогих">От дорогих к дешевым</option>
-                    </select>
+                    <multiselect
+                      v-model="sorting"
+                      :options="['По цене', 'По рейтингу', 'Акционные', 'По новизне', 'По популярности', 'От дешевых к дорогим', 'От дорогих к дешевым']"
+                      :searchable="false"
+                      :allowEmpty="false"
+                      :showLabels="false"
+                      class="sorting"
+                    >
+                      <i
+                        class="fa fa-angle-down caret"
+                        slot="caret"
+                        slot-scope="{ toggle }"
+                        aria-hidden="true"
+                        @mousedown.prevent.stop="toggle"
+                      />
+                    </multiselect>
                   </div>
                 </div>
                 <div class="d-flex align-items-center">
                   <h6>Вид:</h6>
-                  <div class="page-view" @change="val => activePage = val">
-                    <button class="strings" @click="$emit('change', 1)">
+                  <div class="page-view">
+                    <button
+                    class="strings"
+                    :class="{ 'active': activePage === 1}"
+                    @click="activePage = 1"
+                    >
                       <span></span>
                       <span></span>
                       <span></span>
                     </button>
-                    <button class="blocks" @click="$emit('change', 2)">
+                    <button
+                      class="blocks"
+                      :class="{ 'active': activePage === 2}"
+                      @click="activePage = 2"
+                    >
                       <div class="d-flex justify-content-between mb-1">
                         <span></span>
                         <span></span>
@@ -200,41 +240,18 @@
               </div>
               <div class="produkts">
                 <div class="product-strings active" v-if="activePage === 1">
-                  <ProductStrings />
-                  <ProductStrings />
-                  <ProductStrings />
-                  <ProductStrings />
-                  <ProductStrings />
-                  <ProductStrings />
-                  <ProductStrings />
+                  <ProductStrings
+                    v-for="index in productCount"
+                    :key="index"
+                  />
                 </div>
                 <div class="product-blocks" v-if="activePage === 2">
                   <div class="row">
-                    <div class="col-4">
-                      <ProductBlocks />
-                    </div>
-                    <div class="col-4">
-                      <ProductBlocks />
-                    </div>
-                    <div class="col-4">
-                      <ProductBlocks />
-                    </div>
-                    <div class="col-4">
-                      <ProductBlocks />
-                    </div>
-                    <div class="col-4">
-                      <ProductBlocks />
-                    </div>
-                    <div class="col-4">
-                      <ProductBlocks />
-                    </div>
-                    <div class="col-4">
-                      <ProductBlocks />
-                    </div>
-                    <div class="col-4">
-                      <ProductBlocks />
-                    </div>
-                    <div class="col-4">
+                    <div
+                      class="col-4"
+                      v-for="index in productCount"
+                      :key="index"
+                    >
                       <ProductBlocks />
                     </div>
                   </div>
@@ -256,7 +273,7 @@
                     <BtnBlue text="След"/>
                   </div>
                 </div>
-                <button class="btn-repeat">
+                <button class="btn-repeat" @click="productCount = productCount.concat(new Array(9))">
                   <i class="fa fa-repeat" aria-hidden="true"></i>
                   Показать больше товаров
                 </button>
@@ -292,7 +309,15 @@ export default {
   data: () => ({
     marka: 'BMW',
     isMenuOpen: true,
-    activePage: 1
+    activePage: 1,
+    sorting: 'По цене',
+    productCount: new Array(9),
+    collapsedMenu: {
+      0: true,
+      1: true,
+      2: false,
+      3: false
+    }
   })
   // $('.range-slider').parents('.range-slider-wrap').find('.from').val(2500 + ' грн');
   // $('.range-slider').parents('.range-slider-wrap').find('.to').val(25000 + ' грн');
@@ -434,8 +459,10 @@ export default {
           }
         }
         .manufacturer{
+          padding-top: 15px;
           .name-manufacturer{
             margin-bottom: 25px;
+            cursor: pointer;
             h6{
               color: #242424;
               font-size: 16px;
@@ -469,10 +496,11 @@ export default {
         }
         .price{
           border-top: 1px solid rgba(40, 40, 40, 0.25);
-          padding: 25px 0 0;
-          margin-bottom: 10px;
+          margin-top: 10px;
+          padding-top: 15px;
           .name-price{
             margin-bottom: 25px;
+            cursor: pointer;
             h6{
               color: #242424;
               font-size: 16px;
@@ -530,9 +558,11 @@ export default {
         }
         .attribute{
           border-top: 1px solid rgba(40, 40, 40, 0.25);
-          padding: 25px 0 0;
+          margin-top: 10px;
+          padding-top: 15px;
           .name-attribute{
             margin-bottom: 25px;
+            cursor: pointer;
             h6{
               color: #242424;
               font-size: 16px;
@@ -574,47 +604,47 @@ export default {
           font-size: 16px;
           font-weight: 400;
           margin: 0;
+          line-height: 1.4;
         }
         .output-by{
-          .nice-select {
-            width: 100%;
-            background: none;
-            color: #00b9e5;
-            font-size: 16px;
-            font-weight: 400;
-            padding-left: 9px;
-            padding-right: 15px;
-            display: flex;
-            align-items: center;
-            border: none;
-            .option{
-              text-transform: none;
-              font-size: 15px;
-              line-height: 36px;
-              color: #313131;
-              min-height: 35px;
-              padding-left: 25px;
-              padding-right: 70px;
-              &:hover{
-                background-color: #fff;
+          .sorting {
+            min-height: unset;
+            .caret {
+              position: absolute;
+              top: 50%;
+              right: 10px;
+              transform: translateY(-50%);
+              transition: all .3s;
+              color: #00b9e5;
+            }
+            &.multiselect--active {
+              .caret {
+                transform: translateY(-50%) rotate(180deg);
               }
             }
-            .list {
-              min-width: 100%;
-              .option.selected{
-                position: relative;
-                &::before{
-                  position: absolute;
-                  content: '\f00c';
-                  font-family: 'FontAwesome';
-                  right: 10px;
-                  top: 50%;
-                  transform: translateY(-50%);
-                }
+            /deep/ .multiselect__tags {
+              padding-top: 0;
+              border: none;
+              background: none;
+              min-height: unset;
+              .multiselect__single {
+                background: none;
+                margin-bottom: 0;
+                color: #00b9e5;
+                border-radius: 0;
+                padding-left: 0;
+                border-bottom: 1px dashed #00b9e5;
               }
             }
-            &:focus{
-              outline: none;
+            /deep/ .multiselect__content-wrapper {
+              min-width: 230px;
+              .multiselect__option--highlight {
+                background: #00b9e5;
+              }
+              .multiselect__option--selected {
+                background: #fff;
+                color: #000;
+              }
             }
           }
         }
@@ -647,6 +677,11 @@ export default {
             &:focus{
               outline: none;
             }
+            &.active {
+              span {
+                background-color: #00b9e5;
+              }
+            }
           }
           .blocks{
             border: none;
@@ -655,7 +690,7 @@ export default {
               display: block;
               width: 9px;
               height: 9px;
-              background-color: #00b9e5;
+              background-color: rgba(36, 36, 36, 0.49);
               margin-right: 6px;
               transition: all 0.3s;
             }
@@ -666,6 +701,11 @@ export default {
             }
             &:focus{
               outline: none;
+            }
+            &.active {
+              span {
+                background-color: #00b9e5;
+              }
             }
           }
         }
@@ -734,6 +774,21 @@ export default {
           line-height: 22px;
           opacity: 0.64;
         }
+      }
+    }
+  }
+  .collapse-section {
+    max-height: 40px;
+    overflow: hidden;
+    transition: all .3s;
+    .fa-angle-down {
+      transform: rotate(180deg);
+      transition: all .3s;
+    }
+    &.open {
+      max-height: 500px;
+      .fa-angle-down {
+        transform: rotate(0);
       }
     }
   }
